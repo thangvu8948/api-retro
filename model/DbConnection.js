aedeea6e20cd5dd7c1fedd1e7bf639c45a317b3e
Đức Thắng <thangvu8948@gmail.com>
2020-10-18 02:13:46 +0700

    + config to cleardb

--- model/DbConnection.js
+++ model/DbConnection.js
@@ -1,10 +1,9 @@
 var mysql = require("mysql");
 var connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password:'1111',
-    database: 'sakila',
-    port:'3306'
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'bda2d2f6223b5b',
+    password:'eb763e75',
+    database: 'heroku_af196c9c76bbcff'
 })
 
 module.exports = connection;
