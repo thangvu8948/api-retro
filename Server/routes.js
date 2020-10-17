cb1bc0b4d0ceb8e3bf81e49f524211ffd9d6e2af
Đức Thắng <thangvu8948@gmail.com>
2020-10-18 01:45:30 +0700

    + change content routes.js

--- Server/routes.js
+++ Server/routes.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var film = require('../model/Film');
 
 router.get('/', (req, res, next) => {
-    film.getAllFilm((err, rows) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(rows);
-        }
-    })
+    // film.getAllFilm((err, rows) => {
+    //     if (err) {
+    //         res.json(err);
+    //     } else {
+    //         res.json(rows);
+    //     }
+    // })
     res.send("Hello World");
 })
 module.exports = router;
